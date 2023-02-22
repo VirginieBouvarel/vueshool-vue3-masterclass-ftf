d54b823fa03de9d9175fdb42e36649fa71daa1cd
Virginie <vb@ikimo9.com>
2023-02-22 11:43:05 +0100

    add PageHome

---
+++ src/components/PageHome.vue
@@ -0,0 +1,28 @@
+<script setup>
+import sourceData from "@/data.json";
+import { reactive } from "vue";
+
+const threads = reactive(sourceData.threads);
+const posts = reactive(sourceData.posts);
+const users = reactive(sourceData.users);
+
+function postById(postId) {
+  return posts.find((post) => post.id === postId);
+}
+function userById(userId) {
+  return users.find((user) => user.id === userId);
+}
+</script>
+
+<template>
+  <div>Hello from Page Home</div>
+  <div v-for="thread in threads" :key="thread.id">
+    <h2>{{ thread.title }}</h2>
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>{{ userById(postById(postId).userId).name }}</p>
+      <p>{{ postById(postId).text }}</p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
