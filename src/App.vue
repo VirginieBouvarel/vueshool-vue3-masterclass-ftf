472b3d3257091d5ffcb25b229802a4418958b2bd
Virginie <vb@ikimo9.com>
2023-02-22 14:24:46 +0100

    add global styles

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,12 @@ import PageHome from "./components/PageHome.vue";
 </script>
 
 <template>
-  <img
-    alt="Vue logo"
-    class="logo"
-    src="@/assets/logo.svg"
-    width="125"
-    height="125"
-  />
-
-  <PageHome />
+  <img alt="Vue logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="container">
+    <PageHome />
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
+<style>
+@import "assets/style.css";
 </style>
