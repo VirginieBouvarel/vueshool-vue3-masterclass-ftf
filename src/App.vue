7dbac9f45947da042fe1676094b428b4a5ee2141
Virginie <vb@ikimo9.com>
2023-02-28 10:17:45 +0100

    C3V18 - install vue-router
    Install and Configure Vue Router 4

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
-<script setup>
-import PageHome from "./components/PageHome.vue";
-</script>
+<script setup></script>
 
 <template>
   <img alt="Vue logo" src="@/assets/logo.svg" width="125" height="125" />
   <div class="container">
-    <PageHome />
+    <router-view></router-view>
   </div>
 </template>
 
