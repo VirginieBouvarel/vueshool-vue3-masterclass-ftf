97e7aa12916cdd8af953d60671eaf242adcfb89a
Virginie <vb@ikimo9.com>
2023-03-07 21:17:05 +0100

    C4V27 - ajout PostList
    Creating a Forum Post Component

---
+++ src/components/PostList.vue
@@ -0,0 +1,53 @@
+<script setup>
+import sourceData from "@/data.json";
+import { reactive } from "vue";
+
+const props = defineProps({
+  posts: {
+    type: Array,
+    required: true,
+  },
+});
+
+const users = reactive(sourceData.users);
+
+function userById(userId) {
+  return users.find((user) => user.id === userId);
+}
+</script>
+
+<template>
+  <div class="post-list">
+    <div class="post" v-for="post in props.posts" :key="post.id">
+      <div class="user-info">
+        <a href="#" class="user-name">{{ userById(post.userId).name }}</a>
+
+        <a href="#">
+          <img
+            class="avatar-large"
+            :src="userById(post.userId).avatar"
+            alt=""
+            width="95"
+            height="95"
+          />
+        </a>
+
+        <p class="desktop-only text-small">107 posts</p>
+      </div>
+
+      <div class="post-content">
+        <div>
+          <p>
+            {{ post.text }}
+          </p>
+        </div>
+      </div>
+
+      <div class="post-date text-faded">
+        {{ post.publishedAt }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -1,6 +1,7 @@
 <script setup>
 import sourceData from "@/data.json";
 import { reactive, computed } from "vue";
+import PostList from "@/components/PostList.vue";
 
 const props = defineProps({
   id: {
@@ -11,55 +12,18 @@ const props = defineProps({
 
 const threads = reactive(sourceData.threads);
 const posts = reactive(sourceData.posts);
-const users = reactive(sourceData.users);
 
 const thread = computed(() => threads.find((thread) => thread.id === props.id));
-
-function postById(postId) {
-  return posts.find((post) => post.id === postId);
-}
-function userById(userId) {
-  return users.find((user) => user.id === userId);
-}
+const threadPosts = computed(() =>
+  posts.filter((post) => post.threadId === props.id)
+);
 </script>
 
 <template>
   <div v-if="thread" class="col-large push-top">
     <h1>{{ thread.title }}</h1>
 
-    <div class="post-list">
-      <div class="post" v-for="postId in thread.posts" :key="postId">
-        <div class="user-info">
-          <a href="#" class="user-name">{{
-            userById(postById(postId).userId).name
-          }}</a>
-
-          <a href="#">
-            <img
-              class="avatar-large"
-              :src="userById(postById(postId).userId).avatar"
-              alt=""
-              width="95"
-              height="95"
-            />
-          </a>
-
-          <p class="desktop-only text-small">107 posts</p>
-        </div>
-
-        <div class="post-content">
-          <div>
-            <p>
-              {{ postById(postId).text }}
-            </p>
-          </div>
-        </div>
-
-        <div class="post-date text-faded">
-          {{ postById(postId).publishedAt }}
-        </div>
-      </div>
-    </div>
+    <post-list :posts="threadPosts" />
   </div>
 </template>
 
