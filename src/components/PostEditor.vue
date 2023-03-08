5bc01cd393143862ad316047b47612c69d28db87
Virginie <vb@ikimo9.com>
2023-03-08 10:25:53 +0100

    C4V29 - ajout PostEditor
    Communication Between Components

---
+++ src/components/PostEditor.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="col-full">
+    <form @submit.prevent="save">
+      <div class="form-group">
+        <textarea
+          class="form-input"
+          name="content"
+          rows="10"
+          cols="30"
+          v-model="text"
+        ></textarea>
+      </div>
+
+      <div class="form-actions">
+        <button class="btn-blue">Submit post</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const emit = defineEmits(["save"]);
+
+const text = ref("");
+
+function save() {
+  const postId = "gggg" + Math.round();
+  const post = {
+    id: postId,
+    text: text.value,
+    publishedAt: Math.floor(Date.now() / 1000),
+    userId: "rpbB8C6ifrYmNDufMERWfQUoa202",
+  };
+
+  emit("save", { post });
+  text.value = "";
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -3,38 +3,20 @@
     <h1>{{ thread.title }}</h1>
 
     <post-list :posts="threadPosts" />
-
-    <div class="col-full">
-      <form @submit.prevent="addPost">
-        <div class="form-group">
-          <textarea
-            id=""
-            class="form-input"
-            name="content"
-            rows="10"
-            cols="30"
-            v-model="newPostText"
-          ></textarea>
-        </div>
-
-        <div class="form-actions">
-          <button class="btn-blue">Submit post</button>
-        </div>
-      </form>
-    </div>
+    <post-editor @save="addPost" />
   </div>
 </template>
 
 <script setup>
 import sourceData from "@/data.json";
-import { ref, reactive, computed } from "vue";
+import { reactive, computed } from "vue";
 import PostList from "@/components/PostList.vue";
+import PostEditor from "@/components/PostEditor.vue";
 
 const props = defineProps({
   id: { type: String, required: true },
 });
 
-const newPostText = ref("");
 const threads = reactive(sourceData.threads);
 const posts = reactive(sourceData.posts);
 
@@ -43,18 +25,15 @@ const threadPosts = computed(() =>
   posts.filter((post) => post.threadId === props.id)
 );
 
-function addPost() {
-  const postId = "gggg" + Math.round();
+function addPost(event) {
   const post = {
-    id: postId,
-    text: newPostText.value,
-    publishedAt: Math.floor(Date.now() / 1000),
+    ...event.post,
     threadId: props.id,
-    userId: "rpbB8C6ifrYmNDufMERWfQUoa202",
   };
   posts.push(post);
-  thread.value.posts.push(postId);
-  newPostText.value = "";
+  thread.value.posts.push(post.id);
+
+  console.log(post);
 }
 </script>
 
