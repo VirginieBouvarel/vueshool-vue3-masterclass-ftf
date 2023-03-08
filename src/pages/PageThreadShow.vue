6b518e186f86fcba6a534dd104902c6719b2f30f
Virginie <vb@ikimo9.com>
2023-03-08 09:21:30 +0100

    C4V28 - PageThreadShow ajout formulaire
    Creating a Post Editor

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -1,15 +1,13 @@
 <script setup>
 import sourceData from "@/data.json";
-import { reactive, computed } from "vue";
+import { ref, reactive, computed } from "vue";
 import PostList from "@/components/PostList.vue";
 
 const props = defineProps({
-  id: {
-    type: String,
-    required: true,
-  },
+  id: { type: String, required: true },
 });
 
+const newPostText = ref("");
 const threads = reactive(sourceData.threads);
 const posts = reactive(sourceData.posts);
 
@@ -17,6 +15,20 @@ const thread = computed(() => threads.find((thread) => thread.id === props.id));
 const threadPosts = computed(() =>
   posts.filter((post) => post.threadId === props.id)
 );
+
+function addPost() {
+  const postId = "gggg" + Math.round();
+  const post = {
+    id: postId,
+    text: newPostText.value,
+    publishedAt: Math.floor(Date.now() / 1000),
+    threadId: props.id,
+    userId: "rpbB8C6ifrYmNDufMERWfQUoa202",
+  };
+  posts.push(post);
+  thread.value.posts.push(postId);
+  newPostText.value = "";
+}
 </script>
 
 <template>
@@ -24,6 +36,25 @@ const threadPosts = computed(() =>
     <h1>{{ thread.title }}</h1>
 
     <post-list :posts="threadPosts" />
+
+    <div class="col-full">
+      <form @submit.prevent="addPost">
+        <div class="form-group">
+          <textarea
+            id=""
+            class="form-input"
+            name="content"
+            rows="10"
+            cols="30"
+            v-model="newPostText"
+          ></textarea>
+        </div>
+
+        <div class="form-actions">
+          <button class="btn-blue">Submit post</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
