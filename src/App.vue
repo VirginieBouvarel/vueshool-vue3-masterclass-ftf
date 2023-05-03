60339bf00ab60d2377206c8494252b24292b55e1
Virginie <vb@ikimo9.com>
2023-05-03 09:50:52 +0200

    C5V41 - Add TheNavbar
    Creating a Forum Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <img alt="Vue logo" src="@/assets/logo.svg" width="125" height="125" />
+  <the-navbar />
   <div class="container">
     <router-view></router-view>
   </div>
