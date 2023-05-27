e84b0b75cf729ad2d1d92d45df49ab38b8927d92
Virginie <vb@ikimo9.com>
2023-05-27 08:32:12 +0200

    C5V44-45 addUserProfileCard
    - Creating UserProfileCard component
    - Extracting posts and threads from PageProfile to store
    - Defining getters to retrieve properties from another store in a store

--- src/pages/PageProfile.vue
+++ src/pages/PageProfile.vue
@@ -2,38 +2,7 @@
   <div class="container">
     <div class="flex-grid">
       <div class="col-3 push-top">
-        <div class="profile-card">
-          <p class="text-center">
-            <img
-              :src="user.avatar"
-              :alt="`${user.name} profile picture`"
-              class="avatar-xlarge"
-              width="30"
-              height="30"
-            />
-          </p>
-
-          <h1 class="title">{{ user.username }}</h1>
-
-          <p class="text-lead">{{ user.name }}</p>
-
-          <p class="text-justify">{{ user.bio || "No bio specified." }}</p>
-
-          <span class="online">{{ user.username }} is online</span>
-
-          <div class="stats">
-            <span>{{ userPostsCount }} posts</span>
-            <span>{{ userThreadsCount }} threads</span>
-          </div>
-
-          <hr />
-
-          <p v-if="user.website" class="text-large text-center">
-            <i class="fa fa-globe"></i>
-            <a :href="user.website">{{ user.website }}</a>
-          </p>
-        </div>
-
+        <UserProfileCard :user="user" />
         <p class="text-xsmall text-faded text-center">
           Member since june 2003, last visited 4 hours ago
         </p>
@@ -52,31 +21,13 @@
           <a href="#">See only started threads?</a>
         </div>
         <hr />
-        <PostList :posts="userPosts" />
+        <PostList :posts="user.posts" />
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
 import { useUsersStore } from "@/stores/UsersStore";
-import { usePostsStore } from "@/stores/PostsStore";
-import { useThreadsStore } from "@/stores/ThreadsStore";
-
 const user = useUsersStore().authUser;
-
-const postsStore = usePostsStore();
-const userPosts = computed(() =>
-  postsStore.posts.filter((post) => post.userId === user.id)
-);
-const userPostsCount = computed(() => userPosts.value.length);
-
-const threadsStore = useThreadsStore();
-const userThreads = computed(() =>
-  threadsStore.threads.filter((thread) => thread.userId === user.id)
-);
-const userThreadsCount = computed(() => userThreads.value.length);
 </script>
-
-<style lang="scss" scoped></style>
--- src/stores/UsersStore.js
+++ src/stores/UsersStore.js
@@ -1,4 +1,6 @@
 import { defineStore, acceptHMRUpdate } from "pinia";
+import { usePostsStore } from "@/stores/PostsStore";
+import { useThreadsStore } from "@/stores/ThreadsStore";
 import sourceData from "@/data";
 
 export const useUsersStore = defineStore("UsersStore", {
@@ -9,7 +11,35 @@ export const useUsersStore = defineStore("UsersStore", {
     };
   },
   getters: {
-    authUser: (state) => state.users.find((user) => user.id === state.authId),
+    authUser: (state) => {
+      const user = state.users.find((user) => user.id === state.authId);
+      if (!user) return null;
+
+      const postsStore = usePostsStore();
+      const threadsStore = useThreadsStore();
+
+      return {
+        ...user,
+        // authuser.posts
+        get posts() {
+          return postsStore.posts.filter((post) => post.userId === user.id);
+        },
+        // authuser.postsCount
+        get postsCount() {
+          return this.posts.length;
+        },
+        // authuser.posts
+        get threads() {
+          return threadsStore.threads.filter(
+            (thread) => thread.userId === user.id
+          );
+        },
+        // authuser.posts
+        get threadsCount() {
+          return this.threads.length;
+        },
+      };
+    },
   },
   actions: {},
 });
