bbc199948698b4e1a3cd97f9f40181da4b406656
Virginie <vb@ikimo9.com>
2023-08-07 17:26:33 +0200

    C6V56 - add Edit Thread button
    Improving the ThreadEditor

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -1,6 +1,15 @@
 <template>
   <div v-if="thread" class="col-large push-top">
-    <h1>{{ thread.title }}</h1>
+    <h1>
+      {{ thread.title }}
+      <router-link
+        :to="{ name: 'ThreadEdit', id: props.id }"
+        class="btn-green btn-small"
+        tag="button"
+      >
+        Edit Thread
+      </router-link>
+    </h1>
 
     <post-list :posts="threadPosts" />
     <post-editor @save="addPost" />
