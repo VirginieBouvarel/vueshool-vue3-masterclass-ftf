8199ad70d6c8219e01611eb1233de44854793419
Virginie <vb@ikimo9.com>
2023-09-12 10:20:25 +0200

    C9V86 - Hiding Content Until Data is Fully Loaded

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,21 +1,27 @@
 <template>
-  <h1>Welcome to the Forum</h1>
-  <CategoryList :categories="categories" />
+  <div v-if="ready" class="container">
+    <h1>Welcome to the Forum</h1>
+    <CategoryList :categories="categories" />
+  </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useCategoriesStore } from "@/stores/CategoriesStore";
 import { useForumsStore } from "@/stores/ForumsStore";
 const categoriesStore = useCategoriesStore();
 const forumsStore = useForumsStore();
 
+const ready = ref(false);
+
 const categories = computed(() => {
   return categoriesStore.categories;
 });
 
-await categoriesStore.fetchAllCategories();
-
-const forumsIds = categories.value.map((category) => category.forums).flat();
-forumsStore.fetchForums({ ids: forumsIds });
+(async () => {
+  await categoriesStore.fetchAllCategories();
+  const forumsIds = categories.value.map((category) => category.forums).flat();
+  await forumsStore.fetchForums({ ids: forumsIds });
+  ready.value = true;
+})();
 </script>
