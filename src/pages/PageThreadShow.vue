15328b853f5c853bb032137d3a94ce133a5182c7
Virginie <vb@ikimo9.com>
2023-11-18 18:45:18 +0100

    C12V111 - Protect Create and Edit Posts
    Allowing Only Authenticated Users to Create and Edit Posts

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -2,7 +2,10 @@
   <div v-if="thread" class="col-large push-top">
     <h1>
       {{ thread.title }}
-      <router-link :to="{ name: 'ThreadEdit', id: props.id }">
+      <router-link
+        v-if="thread.userId === usersStore.authUser?.id"
+        :to="{ name: 'ThreadEdit', id: props.id }"
+      >
         <button class="btn-green btn-small">Edit Thread</button>
       </router-link>
     </h1>
@@ -19,7 +22,19 @@
     </p>
 
     <post-list :posts="threadPosts" />
-    <post-editor @save="addPost" />
+    <post-editor v-if="usersStore.authUser" @save="addPost" />
+    <div v-else class="text-center" style="margin-bottom: 50px">
+      <router-link :to="{ name: 'SignIn', query: { redirectTo: route.path } }">
+        Sign In
+      </router-link>
+      or
+      <router-link
+        :to="{ name: 'Register', query: { redirectTo: route.path } }"
+      >
+        Register
+      </router-link>
+      to reply.
+    </div>
   </div>
 </template>
 
@@ -28,10 +43,12 @@ import { computed } from "vue";
 import { useThreadsStore } from "@/stores/ThreadsStore";
 import { usePostsStore } from "@/stores/PostsStore";
 import { useUsersStore } from "@/stores/UsersStore";
+import { useRoute } from "vue-router";
 
 const threadsStore = useThreadsStore();
 const postsStore = usePostsStore();
 const usersStore = useUsersStore();
+const route = useRoute();
 
 const props = defineProps({
   id: { type: String, required: true },
@@ -45,7 +62,6 @@ await usersStore.fetchUsers({ ids: usersIds });
 const thread = computed(() => {
   return threadsStore.thread(props.id);
 });
-
 const threadPosts = computed(() =>
   postsStore.posts.filter((post) => post.threadId === props.id)
 );
